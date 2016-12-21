eae9023846dbd37eca5fd4cbe7529eaf98725054
Dobrohotova <irina.dobrohotova@gmail.com>
2016-12-22 00:42:09 +0100

    Trying to solve it..

--- scripts/homeworkcalculator.js
+++ scripts/homeworkcalculator.js
@@ -6,7 +6,11 @@ var znak = document.querySelector('.operacija');
 var fin = document.querySelector('.ednakvo');
 var result = document.querySelector('.reshenie');
 
-result.addEventListener('click', function(){
+// na result ili na fin kje treba da se stavi eventListener?! Kako da se 
+// kazhe deka sakash koga kje klikne na fin(button) rezultatot da go dava
+// vo result
+
+fin.addEventListener('click', function(){
 	if(znak == '+'){
 			return (eden + dva);
 		} else if(znak == '/'){
@@ -16,6 +20,10 @@ result.addEventListener('click', function(){
 		} else if(znak == '*'){
 			return (eden * dva);
 	}
+}
+
+function answer(){
+	result.value = eval();
 }
 
 }
