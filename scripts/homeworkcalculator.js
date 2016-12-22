b43a1f69bd895c5a576e1ccb97c5938062d46421
Dobrohotova <irina.dobrohotova@gmail.com>
2016-12-22 01:14:23 +0100

    Trying out again smth more

--- scripts/homeworkcalculator.js
+++ scripts/homeworkcalculator.js
@@ -1,29 +1,51 @@
+// function calculate(){
+
+// var eden = document.querySelector('.prvbox').value;
+// var dva = document.querySelector('.vtorbox').value;
+// var znak = document.querySelector('.operacija');
+// var fin = document.querySelector('.ednakvo');
+// var result = document.querySelector('.reshenie');
+
+// // na result ili na fin kje treba da se stavi eventListener?! Kako da se 
+// // kazhe deka sakash koga kje klikne na fin(button) rezultatot da go dava
+// // vo result
+
+// fin.addEventListener('click', function(){
+// 	if(znak == '+'){
+// 			return (eden + dva);
+// 		} else if(znak == '/'){
+// 			return (eden / dva);
+// 		} else if(znak == '-'){
+// 			return (eden - dva);
+// 		} else if(znak == '*'){
+// 			return (eden * dva);
+// 	}
+// }
+
+// function answer(){
+// 	result.value = eval();
+// }
+
+// }
+
+
+function plus(){
+	document.value += '+';
+}
+
+function minus(){
+	document.value -= '-';
+}
+
+function multiply(){
+	document.value *= '*';
+}
+
+function divide(){
+	document.value /= '/';
+}
+
 function calculate(){
-
-var eden = document.querySelector('.prvbox').value;
-var dva = document.querySelector('.vtorbox').value;
-var znak = document.querySelector('.operacija');
-var fin = document.querySelector('.ednakvo');
-var result = document.querySelector('.reshenie');
-
-// na result ili na fin kje treba da se stavi eventListener?! Kako da se 
-// kazhe deka sakash koga kje klikne na fin(button) rezultatot da go dava
-// vo result
-
-fin.addEventListener('click', function(){
-	if(znak == '+'){
-			return (eden + dva);
-		} else if(znak == '/'){
-			return (eden / dva);
-		} else if(znak == '-'){
-			return (eden - dva);
-		} else if(znak == '*'){
-			return (eden * dva);
-	}
-}
-
-function answer(){
-	result.value = eval();
-}
-
+	var calculate = eval(document.value);
+	document.value = calculate
 }
