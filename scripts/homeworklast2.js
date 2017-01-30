4cf870d71d26582df9e268e2d5cafd8b7bb3acce
Dobrohotova <irina.dobrohotova@gmail.com>
2017-01-31 00:44:15 +0100

    Homeworklast done with css and some starting point for the other one

---
+++ scripts/homeworklast2.js
@@ -0,0 +1,42 @@
+var var_selektor = document.getElementById("select_gradovi");
+
+var city=var_selektor.options[var_selektor.selectedIndex].value;
+getWeatherByCityName(city);
+
+var_selektor.addEventListener("change", function(){
+    			var city=var_selektor.options[var_selektor.selectedIndex].value;
+				getWeatherByCityName(city);
+
+});
+
+
+function getWeatherByCityName(city){
+			var url = 'http://airquality.moepp.gov.mk/graphs/site/pages/MakeGraph.php?graph=StationLineGraph&station='+city+'&parameter=PM10&endDate=2016-12-29&timeMode=Week&background=false&i=1483013484461&lang=mk';
+			fetch(url).then(function(result){
+			   result.json().then(function(data){
+				   var body = document.querySelector('body');
+
+				});
+			})
+}
+
+
+// if (city == Skopje) {
+// 	var SkopjeUrl = 'http://airquality.moepp.gov.mk/graphs/site/pages/MakeGraph.php?graph=StationLineGraph&station=Centar&parameter=PM10&endDate=2016-12-29&timeMode=Week&background=false&i=1483013484461&lang=mk';
+// } else if (city == Veles) {
+// 	var VelesUrl = 'http://airquality.moepp.gov.mk/graphs/site/pages/MakeGraph.php?graph=StationLineGraph&station=Veles1&parameter=PM10&endDate=2016-12-29&timeMode=Week&background=false&i=1483013674742&lang=mk';
+// }
+
+
+if (pm10 > 50) {
+	alert('Opasnost!!');
+		var img = document.createElement("img");
+		img.src = "../media/images/mask-gas-male-man-46796.jpeg";
+		var src = document.querySelector("body");
+		body.appendChild(img);
+// plus da prikazhe rezultat
+} else if (pm10 < 50) {
+		// da si prikazhe rezultat za chesticite
+	} else if (pm10 = 50) {
+		alert('Zagadenosta e na granica! Se preporachuva ....')
+	};
